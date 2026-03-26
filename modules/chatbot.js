1e9c43f401a1b16e370a8dff4d44ab1a5375e997
Hamadine AG MOCTAR <terpoland4@gmail.com>
2026-03-26 20:16:51 +0100

    Update chatbot.js

--- modules/chatbot.js
+++ modules/chatbot.js
@@ -4,17 +4,13 @@ import { normalize } from "./utils.js";
 export function reponseBot(txt) {
   const clean = normalize(txt);
 
-  if (clean.includes("bonjour")) {
-    return "Bonjour 👋";
-  }
+  if (clean.includes("bonjour")) return "Bonjour 👋";
 
   const mot = AppState.vocabulaire.find(v =>
     normalize(clean).includes(normalize(v.mot))
   );
 
-  if (mot) {
-    return `📖 ${mot.mot} → ${mot.fr}`;
-  }
+  if (mot) return `📖 ${mot.mot} → ${mot.fr}`;
 
   return "Je peux t’aider avec le dictionnaire 📖";
 }
