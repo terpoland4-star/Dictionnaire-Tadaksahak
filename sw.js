205afbdfc9713090ee388827a378fef5704e5206
Hamadine AG MOCTAR <terpoland4@gmail.com>
2026-04-10 14:27:06 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -20,27 +20,21 @@ const urlsToCache = [
 self.addEventListener('install', event => {
   console.log('SW installation');
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
 
 self.addEventListener('activate', event => {
   console.log('SW activation');
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
   );
 });
